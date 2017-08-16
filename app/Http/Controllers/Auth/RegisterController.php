048f6f315a1f2a22a9e82912b48a9d6d0fd56d8f
Davi Santos <davi.santos14@fatec.sp.gov.br>
2017-08-16 10:48:39 -0300

    Correção de bug, o usuário cadastrado era logado após o register.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,8 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
 
 class RegisterController extends Controller
 {
@@ -75,5 +77,29 @@ class RegisterController extends Controller
             'setor' => $data['setor'],
             'status' => TRUE,
         ]);
-    }
+    }    /**
+    * Show the application registration form.
+    *
+    * @return \Illuminate\Http\Response
+    */
+   public function showRegistrationForm()
+   {
+       return view('auth.register');
+   }
+
+   /**
+    * Handle a registration request for the application.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
+   public function register(Request $request)
+   {
+       $this->validator($request->all())->validate();
+
+       event(new Registered($user = $this->create($request->all())));
+
+       return $this->registered($request, $user)
+                       ?: redirect($this->redirectPath());
+   }
 }
