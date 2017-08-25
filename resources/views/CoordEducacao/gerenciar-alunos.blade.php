df4e5b3e175223488c60667b8fcb0453e0544d6e
Davi Santos <davi.santos14@fatec.sp.gov.br>
2017-08-25 09:02:56 -0300

    Início dos campos da model aluno

--- resources/views/CoordEducacao/gerenciar-alunos.blade.php
+++ resources/views/CoordEducacao/gerenciar-alunos.blade.php
@@ -4,5 +4,21 @@
     @parent
     @section('tituloPagina', 'ALUNOS REGISTRADOS')
     @section('tituloGuia', 'SIMEBB - Gerenciamento de alunos')
-    
-@endsection
+
+    <div class="row clearfix">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="card">
+                <div class="header">
+                    <h2>
+                        Informações gerais
+                        <small>Detalhes dos alunos cadastrados</small>
+                    </h2>
+                    <br />
+                </div>
+                <div class="body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
